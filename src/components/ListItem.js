ebb1120ebfd1b02b1be13d22805d2bedc48d99dc
mordochi <lynn@portto.com>
2023-04-23 04:22:07 +0800

    feat: add del tag for the completed tasks

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -5,6 +5,7 @@ import DeleteButton from './DeleteButton'
 const { Title } = Typography;
 
 const Container = ({loading,children}) => loading?<Spin spinning={true} style={{width: '100%',}} wrapperClassName="spin-container">{children}</Spin>:<>{children}</>
+const StatusContainer = ({completed,children}) => completed? <del>{children}</del>:<>{children}</>
 
 const ListItem = ({index,item:{name,completed,updating,creating,deleting}={}}) => {
     return (
@@ -12,7 +13,7 @@ const ListItem = ({index,item:{name,completed,updating,creating,deleting}={}}) =
             <div style={{width:'100%',display:'flex',justifyContent:'space-between',alignItems:'center'}}>
                 <div style={{width:'100%',minWidth:'0',display:'flex',alignItems:'center'}}>
                     <ToggleButton index={index} completed={completed} updating={updating} />
-                    <Title level={5} ellipsis style={{margin:'0 0 0 8px',textalign:'left'}}>{name}</Title>
+                    <Title level={5} ellipsis style={{margin:'0 0 0 8px',textalign:'left'}}><StatusContainer completed={completed}>{name}</StatusContainer></Title>
                 </div>
                 <DeleteButton index={index} style={{flex:'1'}} />
             </div>
